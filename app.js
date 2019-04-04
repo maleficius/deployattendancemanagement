f0e4a52484889812f736f9d2185ce579030786ae
maleficius <40161522+maleficius@users.noreply.github.com>
2019-04-04 23:13:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var swaggerDefinition = {
     version: '1.0.0',
     description: 'Demonstrating how to use DiemDanh API with your application',
   },
-  host: 'https://deployattendancemanagement.herokuapp.com',
+  host: 'https://demohost2.herokuapp.com/',
   basePath: '/',
 };
 
