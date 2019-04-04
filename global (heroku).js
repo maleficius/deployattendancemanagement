8f9e8e742e4006e5ff50bc91d66bfeb1001382be
maleficius <40161522+maleficius@users.noreply.github.com>
2019-04-04 23:04:11 +0700

    Update global (heroku).js

--- global (heroku).js
+++ global (heroku).js
@@ -21,7 +21,7 @@ module.exports = {
     //    port:'5432',
     //    database: 'qldd'
     //},
-    host: 'https://deployattendancemanagement.herokuapp.com/',
+    host: 'https://demotest2.herokuapp.com/',
     email_setting: {
         host: 'smtp.office365.com', // Office 365 server
         port: 587, // secure SMTP
