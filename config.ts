12d5d2decd8b6f9b7bcf4ceacdbb91f6440f7a48
maleficius <40161522+maleficius@users.noreply.github.com>
2019-04-04 23:30:10 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
     public apiHost = 'https://demohost2.herokuapp.com/api';
-    public host = 'https://demohost2.herokuapp.com/';
+    public host = 'https://demohost2.herokuapp.com';
 }
